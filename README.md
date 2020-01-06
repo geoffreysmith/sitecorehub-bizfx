44fe0255abf90eba4e6cb742de0cb5e5a810bb2a
Geoffrey Smith <gsmith@sgi-usa.org>
2020-01-05 20:26:41 -0600

    added download.ps1

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # sitecorehub-bizfx
-BizFx Microservice Docker Image and Build
+*Sitecore 9.2 XC*
+
+## BizFX 9.2 Installation Instructions
+
+## Files Needed
+
+> Remove ^M Windows style endings in Sublime by going to View > Line Endings > Unix
+```sh
+#!/bin/sh
+# extract.sh
+for f in $(unzip -l "Sitecore.Commerce.WDP.2019.07-4.0.165.zip"); do
+       if [ $f = "Sitecore.BizFX.SDK.3.0.7.zip" ]
+       then
+           unzip -q -d . Sitecore.Commerce.WDP.2019.07-4.0.165.zip $f
+           unzip -q -d . $f
+           rm $f
+       fi
+       if [ $f = "speak-ng-bcl-0.8.0.tgz" ]
+       then
+           unzip -q -d . Sitecore.Commerce.WDP.2019.07-4.0.165.zip $f
+       fi
+       if [ $f = "speak-styling-0.9.0-r00078.tgz" ]
+       then
+           unzip -q -d . Sitecore.Commerce.WDP.2019.07-4.0.165.zip $f
+       fi
+done;
+rm "Sitecore.Commerce.WDP.2019.07-4.0.165.zip"```
--- install/download.ps1
+++ install/download.ps1
@@ -1,7 +1,21 @@
-$loginResponse = Invoke-WebRequest "https://dev.sitecore.net/api/authorization" -Method Post -Body @{
-                            username   = $SitecoreUsername
-                            password   = $SitecorePassword
-                            rememberMe = $true
+#Original: https://github.com/Sitecore/docker-images/blob/master/modules/SitecoreImageBuilder/SitecoreImageBuilder.psm1
+function Invoke-PackageRestore
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "SitecorePassword")]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$SitecoreUsername
+        ,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$SitecorePassword
+    )
+
+    $loginResponse = Invoke-WebRequest "https://dev.sitecore.net/api/authorization" -Method Post -Body @{username = $SitecoreUsername,
+                                                                                                         password = $SitecorePassword,
+                                                                                                         rememberMe = $true
                         } -SessionVariable "sitecoreDownloadSession" -UseBasicParsing
 
                         if ($null -eq $loginResponse -or $loginResponse.StatusCode -ne 200 -or $loginResponse.Content -eq "false")
